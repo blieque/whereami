866ca48fe900e89af64ae355eb0deb9f3a52d49c
Blieque Mariguan <himself@blieque.co.uk>
2021-03-19 17:32:08 +0000

    feat(games): Redesign the games.html page
    - Redesign the `games.html` page for better extensibility and stricter
      vertical rhythm.
    - Add description to the top of `games.html` to introduce new players.

--- generateGamesHTML.js
+++ generateGamesHTML.js
@@ -6,6 +6,31 @@ import {
   readFileSync,
 } from 'fs';
 
+const URL_BASE = 'https://whereami.blieque.co.uk/';
+
+const DIFFICULTIES = [
+  // 1
+  ['🔵', 'Dead easy'],
+  // 2
+  ['🟢', 'Real Easy'],
+  // 3
+  ['🟢', 'Easy'],
+  // 4
+  ['🟡', 'Easy-ish'],
+  // 5
+  ['🟡', 'Mild'],
+  // 6
+  ['🟠', 'Medium'],
+  // 7
+  ['🟠', 'Tricky'],
+  // 8
+  ['🔴', 'Tough'],
+  // 9
+  ['🔴', 'Hard'],
+  // 10
+  ['⚫', 'Cruel'],
+];
+
 const locations = JSON.parse(readFileSync('locations.json', 'utf8'));
 const games = JSON.parse(readFileSync('games.json', 'utf8'));
 const gamesTemplateHTML = readFileSync('games.template.html', 'utf8');
@@ -16,12 +41,51 @@ const gameLocationIDs = enabledGames.flatMap(game => game.locationIDs.flat());
 const unusedLocationIDs = allLocationIDs
   .filter(locationID => !gameLocationIDs.includes(locationID));
 
+const parseTagShorthand = (input) => {
+  if (
+    typeof input !== 'string' ||
+    input.length === 0
+  ) return null;
+
+  // No attempt to validate element, class or ID names.
+  const parts = input.match(/((?:^|\.|#)[^#.\n\s]+)/g);
+
+  return {
+    name: (
+      !parts[0].startsWith('.') &&
+      !parts[0].startsWith('#')
+    ) ? parts[0] : 'div',
+    id: parts
+      .find(part => part.startsWith('#'))
+      ?.slice(1),
+    class: parts
+      .filter(part => part.startsWith('.'))
+      .map(part => part.slice(1)),
+  };
+};
+
 const h = (...args) => {
-  const name = typeof args[0] === 'string' ? args[0] : null;
+  const element = parseTagShorthand(args[0]);
+
   const attributes = (
     typeof args[1] === 'object' &&
     !Array.isArray(args[1])
-  ) ? args[1] : null;
+  ) ? args[1] : {};
+
+  if (element !== null) {
+    const classNames = [
+      attributes?.class || [],
+      element.class,
+    ].flat().join(' ');
+    if (classNames.length > 0) {
+      attributes.class = classNames;
+    }
+
+    if (typeof attributes.id !== 'string') {
+      attributes.id = element.id;
+    }
+  }
+
   const children = (
     args.find(argument => Array.isArray(argument)) ||
     args.slice(1).filter(argument => typeof argument === 'string').slice(0, 1)
@@ -30,17 +94,17 @@ const h = (...args) => {
   const voidElements = ['area', 'base', 'br', 'col', 'embed', 'hr', 'img', 'input', 'link', 'meta', 'param', 'source', 'track', 'wbr']
 
   const attributeString = attributes != null
-    ? Object.keys(attributes)
-      .filter(key => attributes[key] !== false && attributes[key] != null)
-      .map((key) => {
-        const value = key === 'class' && Array.isArray(attributes[key])
-          ? attributes[key]
+    ? Object.entries(attributes)
+      .filter(([_, value]) => value !== false && value != null)
+      .map(([key, value]) => {
+        const valueString = key === 'class' && Array.isArray(attributes[key])
+          ? value
             .filter(className => typeof className === 'string' && className.length > 0)
             .join(' ')
-          : attributes[key];
-        return attributes[key] === true
+          : value;
+        return value === true
           ? ` ${key}`
-          : ` ${key}="${value}"`;
+          : ` ${key}="${valueString}"`;
       })
       .join('')
     : '';
@@ -51,9 +115,9 @@ const h = (...args) => {
 
   if (args[0] === children) return content;
 
-  return voidElements.includes(name)
-    ? `<${name}${attributeString}>`
-    : `<${name}${attributeString}>${content}</${name}>`;
+  return voidElements.includes(element.name)
+    ? `<${element.name}${attributeString}>`
+    : `<${element.name}${attributeString}>${content}</${element.name}>`;
 };
 
 /**
@@ -85,59 +149,70 @@ const wrapH = function (defaultName, defaultAttributes, defaultChildren) {
   }
 };
 
-const URL_BASE = 'https://whereami.blieque.co.uk/';
+const {
+  code,
+  li,
+} = wrapH('code', 'li');
 
-const DIFFICULTIES = [
-  // 1
-  ['🔵', 'Dead easy'],
-  // 2
-  ['🟢', 'Real Easy'],
-  // 3
-  ['🟢', 'Easy'],
-  // 4
-  ['🟡', 'Easy-ish'],
-  // 5
-  ['🟡', 'Mild'],
-  // 6
-  ['🟠', 'Medium'],
-  // 7
-  ['🟠', 'Tricky'],
-  // 8
-  ['🔴', 'Tough'],
-  // 9
-  ['🔴', 'Hard'],
-  // 10
-  ['⚫', 'Cruel'],
-];
+/**
+ * If `round` is an array, this assumes all locations have the same `name` and
+ * `bonus`.
+ *
+ * @param {Location | Array<Location>} round
+ * @returns {string}
+ */
+const roundAsHTML = (round) => {
+  const locations = Array.isArray(round) ? round : [round];
 
-const locationAsHTML = (location) => {
-  const difficultyPair = DIFFICULTIES[location.difficulty - 1];
-  const difficulty = `${difficultyPair[1]} (${location.difficulty}/10)`;
+  const locationsHTML = locations.map((location) => {
+    const difficultyPair = DIFFICULTIES[location.difficulty - 1];
+    const difficulty = `${difficultyPair[1]} (${location.difficulty}/10)`;
 
-  const url = `${URL_BASE}${location.id}`;
-  const link = h('a', { href: url }, location.id);
+    const url = `${URL_BASE}${location.id}`;
+    const link = h('a', { href: url }, location.id);
 
-  const extra = h(
-    'span',
-    [
-      `${location.flag} ${location.name}`,
-      location.bonus
-        ? ` – ${h('a', { href: `${URL_BASE}bonus/${location.bonus}` }, location.bonus)}`
-        : '',
-    ],
-  );
+    return h(
+      '.location',
+      {
+        class: [
+          location.isEnabled === false ? 'location--isDisabled' : '',
+        ],
+      },
+      [
+        difficulty,
+        ': ',
+        link,
+      ],
+    );
+  });
 
   return h(
-    'div',
-    {
-      class: [
-        location.isEnabled === false ? 'isDisabled' : '',
-      ],
-    },
-    [difficulty, ': ', link, extra],
+    '.round',
+    [
+      Array.isArray(round)
+        ? h('ul', locationsHTML.map(li))
+        : locationsHTML[0],
+      h(
+        'button',
+        h('span', 'Copy'),
+      ),
+      h(
+        '.round__locationName',
+        [
+          `${locations[0].flag} ${locations[0].name}`,
+          locations[0].bonus
+            ? ` – ${h('a', { href: `${URL_BASE}bonus/${locations[0].bonus}` }, locations[0].bonus)}`
+            : '',
+        ]
+      ),
+    ],
   );
 };
 
+/**
+ * @param {string} locationIDOrName
+ * @returns {Location}
+ */
 const getLocationByID = (locationIDOrName) => {
   return locations.find(location => (
     location.id === locationIDOrName ||
@@ -145,31 +220,33 @@ const getLocationByID = (locationIDOrName) => {
   ));
 };
 
-const shorthandLocationAsHTML = (shorthandLocation) => {
-  return Array.isArray(shorthandLocation)
-    ? h('ul', shorthandLocation
-      .map(getLocationByID)
-      .map(locationAsHTML)
-      .map(content => h('li', content))
-      .join('\n'))
-    : locationAsHTML(getLocationByID(shorthandLocation));
+const formatDate = (date) => {
+  const monthAndDate = date.match(/[0-9]{2}-[0-9]{2}$/)?.[0] || '';
+  return (
+    monthAndDate > '12-11' &&
+    monthAndDate <= '12-25'
+  )
+    ? `${date} 🌲`
+    : date;
 };
 
-const gameListHTML = h([
-  h('h1', 'Games'),
-
-  enabledGames.map((game) => {
+const gameListHTML = h(
+  'section.gameList',
+  enabledGames.map((game, i) => {
     return h(
       'article',
       { class: 'game' },
       [
         game.date
-          ? h('p', h('time', { datetime: game.date }, game.date))
+          ? h('p', h(
+            'time',
+            { datetime: game.date },
+            formatDate(game.date),
+          ))
           : [],
 
         h('h2', [
           'Where Am I?',
-          game.title ? ` – ${game.title}` : '',
           h(
             'small',
             { style: 'font-weight: 400' },
@@ -182,52 +259,67 @@ const gameListHTML = h([
               ),
             ],
           ),
+          ...(game.title || (i + 1) % 10 === 0
+            ? [
+              h('br'),
+              h(
+                'span',
+                { style: 'font-weight: 500' },
+                game.title || `Game ${(i + 1)}`,
+              ),
+            ]
+            : []),
         ]),
 
-        h('ol',
+        h(
+          'ol',
           game.locationIDs
-            .map(shorthandLocationAsHTML)
-            .map(content => h('li', [
-              content,
-              h(
-                'button',
-                h('span', 'Copy'),
-              ),
-            ]))
-            .join('\n')
+            .map(round => Array.isArray(round)
+              ? round.map(getLocationByID)
+              : getLocationByID(round))
+            .map(roundAsHTML)
+            .map(li),
         ),
       ],
     );
   }),
-]);
+);
 
-const locatonListHTML = h([
-  h('h1', 'Unused locations'),
-  h('ul', unusedLocationIDs
-    .map(getLocationByID)
-    .map(locationAsHTML)
-    .map(wrapH('li', {}))
+const unusedLocationListHTML = h(
+  'section.unusedLocationList',
+  [
+    h('h2', 'Unused locations'),
+    h('ul', unusedLocationIDs
+      .map(getLocationByID)
+      .map(roundAsHTML)
+      .map(li)
+    ),
+  ],
+);
+
+const fileListHTML = h(
+  'section.fileList',
+  h(
+    'ul',
+    readdirSync('.')
+      .filter(file => file.endsWith('.html') || file.endsWith('.js'))
+      .map(file => h('a', { href: `${URL_BASE}${file}` }, file))
+      .map(code)
+      .map(li),
   ),
-]);
+);
 
-const gamesContentHTML = `
-${gameListHTML}
-
-${unusedLocationIDs.length > 0
-  ? `<hr>${locatonListHTML}`
-  : ''}
-
-<hr>
-<ul>
-  ${readdirSync('.')
-    .filter(file => file.endsWith('.html') || file.endsWith('.js'))
-    .map(file => h('a', { href: `${URL_BASE}${file}` }, file))
-    .map(wrapH('code', {}))
-    .map(wrapH('li', {}))
-    .join('')}
-</ul>
-`;
-
-const gamesDocumentHTML = gamesTemplateHTML.replace('{content}', gamesContentHTML);
+const gamesDocumentHTML = gamesTemplateHTML.replace(
+  '{content}',
+  [
+    gameListHTML,
+    unusedLocationIDs.length > 0
+      ? `${unusedLocationListHTML}`
+      : '',
+    fileListHTML,
+  ]
+    .filter(part => part.length > 0)
+    .join('<hr>'),
+);
 
 writeFileSync('games.html', gamesDocumentHTML);
